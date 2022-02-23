d12469a87239956977634802b2594243ee219db1
Robson Carvalho <robson73904@gmail.com>
2022-02-23 18:49:13 -0300

    arquivos

---
+++ js/home.js
@@ -0,0 +1,60 @@
+const studentData = JSON.parse(localStorage.getItem("sra"));
+
+const student = {
+  name: document.querySelector(".name"),
+  registration: document.querySelector(".registration"),
+  classroom: document.querySelector(".classroom"),
+  course: document.querySelector(".course"),
+  schoolGrade: document.querySelector(".schoolGrade"),
+};
+
+student.name.innerHTML = `<strong>Nome:</strong> ${studentData.name}`;
+student.registration.innerHTML = `<strong>Matrícula:</strong> ${studentData.registration}`;
+student.classroom.innerHTML = `<strong>Sala:</strong> ${studentData.classroom}`;
+student.course.innerHTML = `<strong>Curso:</strong> ${studentData.course}`;
+student.schoolGrade.innerHTML = `<strong>Série:</strong> ${studentData.schoolGrade}°`;
+
+const requirement = async () => {
+  fetch("http://localhost:8080/sra-api/requirement", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + studentData.token,
+    },
+    body: JSON.stringify({
+      registration: studentData.registration,
+      name: studentData.name,
+      classroom: studentData.classroom,
+      course: studentData.course,
+      schoolGrade: studentData.schoolGrade,
+    }),
+  })
+    .then((response) => response.json())
+    .then((body) => checkoutRequirement(body));
+};
+
+const checkoutRequirement = (data) => {
+  if (!data.token) {
+    localStorage.removeItem("sra");
+    window.location.href = "../index.html";
+  }
+  if (!data.requirement) {
+    return document.querySelector(".alert").innerHTML = `
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+      <strong class="text">${data.message}</strong>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  `;
+  }
+  return document.querySelector(".alert").innerHTML = `
+  <div class="alert alert-success alert-dismissible fade show" role="alert">
+    <strong class="text">${data.message}</strong>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
+`;
+};
+
+const output = () => {
+  localStorage.removeItem("sra");
+  window.location.href = "../index.html";
+};
---
+++ js/login.js
@@ -0,0 +1,56 @@
+const form = document.forms.login;
+
+form.addEventListener("submit", (action) => {
+  action.preventDefault();
+  const [registration, password] = form;
+  login(registration.value, password.value);
+});
+
+const login = async (registration, password) => {
+  await fetch("http://localhost:8080/sra-api/login", {
+    method: "POST",
+
+    body: JSON.stringify({
+      registration: registration,
+      password: password,
+    }),
+
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => checkoutAuth(data));
+};
+
+const checkoutAuth = async (data) => {
+  if (data.auth) {
+    return addDataLocalStorage(data);
+  }
+  document.querySelector(".alert").innerHTML = `
+  <div class="alert alert-danger alert-dismissible fade show" role="alert">
+      <strong class="text">${data.message}</strong>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  `;
+  form.reset();
+  form.registration.focus();
+};
+
+const addDataLocalStorage = ({ data }) => {
+  const sraData = {
+    token: data.token,
+    name: data.name,
+    registration: data.registration,
+    course: data.course,
+    classroom: data.classroom,
+    schoolGrade: data.schoolGrade,
+  };
+  localStorage.setItem("sra", JSON.stringify(sraData));
+  redirect();
+};
+
+const redirect = () => {
+  window.location.href = "../home.html";
+};
