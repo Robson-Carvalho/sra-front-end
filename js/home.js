3d5b8221dfd0bfc6630703873b06f7c535b80446
Robson Carvalho <robson73904@gmail.com>
2022-02-24 16:10:03 -0300

    alteração

--- js/home.js
+++ js/home.js
@@ -1,18 +1,27 @@
 const studentData = JSON.parse(localStorage.getItem("sra"));
 
-const student = {
-  name: document.querySelector(".name"),
-  registration: document.querySelector(".registration"),
-  classroom: document.querySelector(".classroom"),
-  course: document.querySelector(".course"),
-  schoolGrade: document.querySelector(".schoolGrade"),
+const output = () => {
+  localStorage.removeItem("sra");
+  window.location.href = "../index.html";
 };
 
-student.name.innerHTML = `<strong>Nome:</strong> ${studentData.name}`;
-student.registration.innerHTML = `<strong>Matrícula:</strong> ${studentData.registration}`;
-student.classroom.innerHTML = `<strong>Sala:</strong> ${studentData.classroom}`;
-student.course.innerHTML = `<strong>Curso:</strong> ${studentData.course}`;
-student.schoolGrade.innerHTML = `<strong>Série:</strong> ${studentData.schoolGrade}°`;
+if (localStorage.getItem("sra") === null) {
+  output();
+} else {
+  const student = {
+    name: document.querySelector(".name"),
+    registration: document.querySelector(".registration"),
+    classroom: document.querySelector(".classroom"),
+    course: document.querySelector(".course"),
+    schoolGrade: document.querySelector(".schoolGrade"),
+  };
+
+  student.name.innerHTML = `<strong>Nome:</strong> ${studentData.name}`;
+  student.registration.innerHTML = `<strong>Matrícula:</strong> ${studentData.registration}`;
+  student.classroom.innerHTML = `<strong>Sala:</strong> ${studentData.classroom}`;
+  student.course.innerHTML = `<strong>Curso:</strong> ${studentData.course}`;
+  student.schoolGrade.innerHTML = `<strong>Série:</strong> ${studentData.schoolGrade}°`;
+}
 
 const requirement = async () => {
   fetch("https://lunch-requirement-system.herokuapp.com/sra-api/requirement", {
@@ -39,22 +48,17 @@ const checkoutRequirement = (data) => {
     window.location.href = "../index.html";
   }
   if (!data.requirement) {
-    return document.querySelector(".alert").innerHTML = `
+    return (document.querySelector(".alert").innerHTML = `
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
       <strong class="text">${data.message}</strong>
       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
-  `;
+  `);
   }
-  return document.querySelector(".alert").innerHTML = `
+  return (document.querySelector(".alert").innerHTML = `
   <div class="alert alert-success alert-dismissible fade show" role="alert">
     <strong class="text">${data.message}</strong>
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   </div>
-`;
-};
-
-const output = () => {
-  localStorage.removeItem("sra");
-  window.location.href = "../index.html";
+`);
 };
